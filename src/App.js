d890bfd7af28966934d0047ea52d8edf13629f37
Manh Quan <mquan129dalat@gmail.com>
2023-08-28 21:31:40 +0700

    Recommit for project

--- src/App.js
+++ src/App.js
@@ -7,61 +7,55 @@ import Search from './Search';
 import HookQuery from './HookQuery';
 
 function App() {
-  const [booksData, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-  const [query, setQuery] = useState([]);
-  const [searchQuery] = HookQuery(query);
-
-  const [mergedBooks, setMergedBooks] = useState([]);
-  const [mapIdBooks, setMapIdBooks] = useState(new Map());
-
-  const getAllBooks = async () => {
-    await getAll().then((res) => {
-      setData(res);
-      setMapIdBooks(createMapOfBooks(res));
-    });
-  };
-
-  const handleUpdateShelf = async (book, shelf) => {
-    await update(book, shelf);
-    getAllBooks();
-  };
-
-  const handleSearchQuery = (e) => {
-    let inputValue = e.target.value;
-    setQuery(inputValue);
-  };
-
-  useEffect(() => {
-    const combiningBooksShelf = searchQuery.map((book) => {
-      if (mapIdBooks.has(book.id)) {
-        return mapIdBooks.get(book.id);
-      } else {
-        return book;
-      }
-    });
-    setMergedBooks(combiningBooksShelf);
-  }, [searchQuery]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  const createMapOfBooks = (books) => {
+  function initMapBook(books) {
     const map = new Map();
     books.map((book) => map.set(book.id, book));
     return map;
-  };
+  }
 
-  console.log(booksData[0]);
+  const [mapBook, setMapBook] = useState(new Map());
+
+  async function initBooks() {
+    await getAll().then((item) => {
+      if (item) {
+        setData(item);
+        setMapBook(initMapBook(item));
+      }
+    });
+  }
+
+  async function handleUpdateShelf (book, shelf) {
+    await update(book, shelf);
+    initBooks();
+  }
   
+  const [query, setQuery] = useState([]);
+  const [searchQuery] = HookQuery(query);
+
+  const handleSearchQuery = (e) => { setQuery(e.target.value) };
+
+  const [componentBook, setComponentBook] = useState([]);
   useEffect(() => {
-    getAllBooks();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    const combiningBooks = searchQuery.map((item) => {
+      if (item && mapBook.has(item.id)) {
+        return mapBook.get(item.id);
+      } else {
+        return item;
+      }
+    });
+    setComponentBook(combiningBooks);
+    initBooks();
+  }, [searchQuery]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Router>
       <div className="app">
         <Routes>
-          <Route path="/search" element={ <Search handleSearchQuery={handleSearchQuery} query={query} handleUpdate={handleUpdateShelf} mergedBooks={mergedBooks}  /> }
+          <Route path="/search" element={ <Search handleSearchQuery={handleSearchQuery} query={query} handleUpdate={handleUpdateShelf} mergedBooks={componentBook}  /> }
           />
-          <Route path="/" element={ <HomePage booksData={booksData} handleUpdate={handleUpdateShelf} /> }
+          <Route path="/" element={ <HomePage booksData={data} handleUpdate={handleUpdateShelf} /> }
           />
         </Routes>
       </div>
